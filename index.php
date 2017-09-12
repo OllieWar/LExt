d07ef77e06037a5a5fd870bc79778d2c8fa9a542
OllieWar <olliewarway@gmail.com>
2017-09-12 15:15:52 +0100

    nav

--- index.php
+++ index.php
@@ -23,22 +23,13 @@
 
 	<body>
 
-		<nav class="navbar navbar-toggleable-md navbar-light bg-faded"
-			<ul class="navbar-nav mr-auto">
-      			<li class="nav-item active">
-        			<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      			</li>
-      			<li class="nav-item">
-        			<a class="nav-link" href="#">Link</a>
-      			</li>
-      			<li class="nav-item">
-       				<a class="nav-link disabled" href="#">Disabled</a>
-      			</li>
-    		</ul>
-    		<form class="form-inline my-2 my-lg-0">
-      			<input class="form-control mr-sm-2" type="text" placeholder="Search">
-      			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    		</form>
+		<nav class="navbar navbar-defualt" role="navigation">
+			<ul class="nav navbar-nav">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">FAQ</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
 		</nav>
 
 
