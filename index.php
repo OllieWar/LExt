dd47a0a6907bf1eece7aa55664f93cf64af0b6ee
OllieWar <olliewarway@gmail.com>
2017-09-12 16:12:57 +0100

    centerNav

--- index.php
+++ index.php
@@ -30,12 +30,14 @@
 
 		<!--Navigation-->
 		<nav class="navbar navbar-default" role="navigation">
-      		<ul class="nav navbar-nav">
-      			<li class="active"><a href="#">Home</a></li>
-      			<li><a href="#">About</a></li>
-      			<li><a href="#">FAQ</a></li>
-      			<li><a href="#">Contact</a></li>
-      		</ul>
+			<div class="container">
+	      		<ul class="nav navbar-nav">
+	      			<li class="active"><a href="#">Home</a></li>
+	      			<li><a href="#">About</a></li>
+	      			<li><a href="#">FAQ</a></li>
+	      			<li><a href="#">Contact</a></li>
+	      		</ul>
+      		</div>
     	</nav>
     	<!--End Navigation-->
 
